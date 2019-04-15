b82f4d1365111ddc5e697f5b989f54aa008aede5
Mark Adamson <mark@adamsonfamily.co.uk>
2019-04-15 21:28:08 +0100

    Working game :)

--- protect the pig/MainWindow.xaml.cs
+++ protect the pig/MainWindow.xaml.cs
@@ -21,12 +21,15 @@ namespace protect_the_pig
     public partial class MainWindow : Window
     {
         private int score;
+        Random random=new Random();
+        private int buttonleft = 1;
+        private int buttontop = 1;
         public MainWindow()
         {
             InitializeComponent();
             System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += dispatcherTimer_Tick;
-            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 5);
             dispatcherTimer.Start();
 
 
@@ -35,15 +38,44 @@ namespace protect_the_pig
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
             var currentmargin = button1.Margin;
-            currentmargin.Left += 1;
-            currentmargin.Top += 1;
+            currentmargin.Left += buttonleft;
+            currentmargin.Top += buttontop;
             button1.Margin = currentmargin;
+            if (currentmargin.Left>769.5)
+            {
+                buttonleft = -1;
+
+            }
+            if (currentmargin.Top >399)
+            {
+                buttontop = -1;
+
+            }
+            if (currentmargin.Left <0)
+            {
+                buttonleft = 1;
+
+            }
+            if (currentmargin.Top <0)
+            {
+                buttontop = 1;
+
+            }
+            if (currentmargin.Left > 275.973 && currentmargin.Left < 499.451 && currentmargin.Top > 147.254 && currentmargin.Top < 272.06 )
+            {
+                Gameover.Visibility = Visibility.Visible;
+                button1.Visibility = Visibility.Hidden;
+
+            }
+           
+
 
         }
 
         private void Button1_OnClick(object sender, MouseEventArgs mouseEventArgs)
         {
-            button1.Visibility = Visibility.Hidden;
+            var margin = new Thickness(random.Next(0, 769), random.Next(0, 399),0,0);
+            button1.Margin=margin;
             score+=1;
             scorelabel.Content = score;
         }
